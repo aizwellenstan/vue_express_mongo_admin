297f847bf7b9e046983b36760774bf5134b6c969
aizwellenstan <aizwellenstan@gmail.com>
2019-09-05 17:45:18 +0900

    all

--- client/src/views/login/index.vue
+++ client/src/views/login/index.vue
@@ -1,42 +1,5 @@
 <template>
-<section>
-    <h1>Login</h1>
-    <div v-if="loggingIn">
-    </div>
-    <div v-if="errorMessage" class="alert alert-danger" role="alert">
-      {{errorMessage}}
-    </div>
-    <form v-if="!loggingIn" @submit.prevent="login()">
-      <div class="form-group">
-        <label for="username">Username</label>
-        <input
-          v-model="user.username"
-          type="text"
-          class="form-control"
-          id="username"
-          aria-describedby="usernameHelp"
-          placeholder="Enter a username" required>
-        <h5 id="usernameHelp" class="form-text text-muted">
-         Enter your username to login.
-        </h5>
-      </div>
-      <div class="form-group">
-        <label for="password">Password</label>
-        <input
-          v-model="user.password"
-          type="password"
-          class="form-control"
-          id="password"
-          aria-describedby="passwordHelp"
-          placeholder="Enter a password" required>
-        <h5 id="passwordHelp" class="form-text text-muted">
-         Enter your password to login.
-        </h5>
-      </div>
-      <button type="submit" class="btn btn-primary">Login</button>
-    </form>
-  </section>
-  <!-- <div class="login-container">
+  <div class="login-container">
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" autocomplete="on" label-position="left">
 
       <div class="title-container">
@@ -48,12 +11,14 @@
           <svg-icon icon-class="user" />
         </span>
         <el-input
-          v-model="user.username"
+          ref="username"
+          v-model="loginForm.username"
+          placeholder="Username"
+          name="username"
           type="text"
-          class="form-control"
-          id="username"
-          aria-describedby="usernameHelp"
-          placeholder="Enter a username" required />
+          tabindex="1"
+          autocomplete="on"
+        />
       </el-form-item>
 
       <el-tooltip v-model="capsTooltip" content="Caps lock is On" placement="right" manual>
@@ -62,19 +27,25 @@
             <svg-icon icon-class="password" />
           </span>
           <el-input
-          v-model="user.password"
-          type="password"
-          class="form-control"
-          id="password"
-          aria-describedby="passwordHelp"
-          placeholder="Enter a password" required />
+            :key="passwordType"
+            ref="password"
+            v-model="loginForm.password"
+            :type="passwordType"
+            placeholder="Password"
+            name="password"
+            tabindex="2"
+            autocomplete="on"
+            @keyup.native="checkCapslock"
+            @blur="capsTooltip = false"
+            @keyup.enter.native="handleLogin"
+          />
           <span class="show-pwd" @click="showPwd">
             <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
           </span>
         </el-form-item>
       </el-tooltip>
 
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;">Login</el-button>
+      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
 
       <div style="position:relative">
         <div class="tips">
@@ -99,28 +70,16 @@
       <br>
       <social-sign />
     </el-dialog>
-  </div> -->
+  </div>
 </template>
 
 <script>
-import Joi from 'joi'
 import { validUsername } from '@/utils/validate'
 import SocialSign from './components/SocialSignin'
-
-const LOGIN_URL = 'http://localhost:5000/auth/login'
-
-const schema = Joi.object().keys({
-  username: Joi.string().regex(/(^[a-zA-Z0-9_]+$)/).min(2).max(30)
-    .required(),
-  password: Joi.string().trim().min(10).required(),
-})
-
 export default {
   name: 'Login',
   components: { SocialSign },
   data() {
-    
-
     const validateUsername = (rule, value, callback) => {
       if (!validUsername(value)) {
         callback(new Error('Please enter the correct user name'))
@@ -136,14 +95,6 @@ export default {
       }
     }
     return {
-      
-    errorMessage: '',
-    loggingIn: false,
-    user: {
-      username: '',
-      password: '',
-    },
-  
       loginForm: {
         username: 'admin',
         password: '111111'
@@ -186,65 +137,6 @@ export default {
     // window.removeEventListener('storage', this.afterQRScan)
   },
   methods: {
-    login() {
-      this.errorMessage = ''
-      if (this.validUser()) {
-        this.loggingIn = true
-        const body = {
-          username: this.user.username,
-          password: this.user.password
-        }
-
-        fetch(LOGIN_URL, {
-          method: 'POST',
-          headers: {
-            'content-type': 'application/json'
-          },
-          body: JSON.stringify(body)
-        }).then((response) => {
-          if (response.ok) {
-            return response.json()
-          }
-          return response.json().then((error) => {
-            throw new Error(error.message)
-          })
-        }).then((result) => {
-          this.loading = true
-          this.$store.dispatch('user/login', this.loginForm)
-            .then(() => {
-              localStorage.setItem('token', 'ImLogin')
-              this.$router.push({ path: this.redirect || '/', query: this.otherQuery })
-              this.loading = false
-            })
-            .catch(() => {
-              this.loading = false
-            })
-          localStorage.token = result.token
-          setTimeout(() => {
-            this.loggingIn = false
-          }, 1000)
-        }).catch((error) => {
-          setTimeout(() => {
-            this.loggingIn = false
-            this.errorMessage = error.message
-          }, 1000)
-        })
-      }
-    },
-    validUser() {
-      const result = Joi.validate(this.user, schema)
-      if (result.error === null) {
-        return true
-      }
-
-      if (result.error.message.includes('username')) {
-        this.errorMessage = 'Username is invalid. 😭'
-      } else {
-        this.errorMessage = 'Password is invalid. 🙈'
-      }
-
-      return false
-    },
     checkCapslock({ shiftKey, key } = {}) {
       if (key && key.length === 1) {
         if (shiftKey && (key >= 'a' && key <= 'z') || !shiftKey && (key >= 'A' && key <= 'Z')) {
@@ -319,24 +211,20 @@ export default {
 <style lang="scss">
 /* 修复input 背景不协调 和光标变色 */
 /* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
-
 $bg:#283443;
 $light_gray:#fff;
 $cursor: #fff;
-
 @supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
   .login-container .el-input input {
     color: $cursor;
   }
 }
-
 /* reset element-ui css */
 .login-container {
   .el-input {
     display: inline-block;
     height: 47px;
     width: 85%;
-
     input {
       background: transparent;
       border: 0px;
@@ -346,14 +234,12 @@ $cursor: #fff;
       color: $light_gray;
       height: 47px;
       caret-color: $cursor;
-
       &:-webkit-autofill {
         box-shadow: 0 0 0px 1000px $bg inset !important;
         -webkit-text-fill-color: $cursor !important;
       }
     }
   }
-
   .el-form-item {
     border: 1px solid rgba(255, 255, 255, 0.1);
     background: rgba(0, 0, 0, 0.1);
@@ -367,13 +253,11 @@ $cursor: #fff;
 $bg:#2d3a4b;
 $dark_gray:#889aa4;
 $light_gray:#eee;
-
 .login-container {
   min-height: 100%;
   width: 100%;
   background-color: $bg;
   overflow: hidden;
-
   .login-form {
     position: relative;
     width: 520px;
@@ -382,19 +266,16 @@ $light_gray:#eee;
     margin: 0 auto;
     overflow: hidden;
   }
-
   .tips {
     font-size: 14px;
     color: #fff;
     margin-bottom: 10px;
-
     span {
       &:first-of-type {
         margin-right: 16px;
       }
     }
   }
-
   .svg-container {
     padding: 6px 5px 6px 15px;
     color: $dark_gray;
@@ -402,10 +283,8 @@ $light_gray:#eee;
     width: 30px;
     display: inline-block;
   }
-
   .title-container {
     position: relative;
-
     .title {
       font-size: 26px;
       color: $light_gray;
@@ -414,7 +293,6 @@ $light_gray:#eee;
       font-weight: bold;
     }
   }
-
   .show-pwd {
     position: absolute;
     right: 10px;
@@ -424,13 +302,11 @@ $light_gray:#eee;
     cursor: pointer;
     user-select: none;
   }
-
   .thirdparty-button {
     position: absolute;
     right: 0;
     bottom: 6px;
   }
-
   @media only screen and (max-width: 470px) {
     .thirdparty-button {
       display: none;
